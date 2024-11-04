67e68f1e57728ece7de280dd4221a7496f18d264
ananaya89 <156047228+ananaya89@users.noreply.github.com>
2024-11-04 10:40:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DigitalClock from './clock.js';
+
+function App() {
+  return (
+    <div className="App" style={{ textAlign: 'center', marginTop: '20%' }}>
+      <h1>Digital Clock</h1>
+      <DigitalClock />
+    </div>
+  );
+}
+
+export default App;
